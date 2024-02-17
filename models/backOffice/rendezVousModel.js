066b3d95d393edb8c6c0de2428fee9271e1c0046
RAHAJASON Aroniaina Saotra <89457263+AroniainaSaotra@users.noreply.github.com>
2024-02-17 09:27:13 +0300

    Recherche des rendez-vous par date et par employe

--- models/backOffice/rendezVousModel.js
+++ models/backOffice/rendezVousModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const rendezVousSchema = mongoose.Schema({
     _id : String,
-    id_details:String,
+    id_detail:String,
     id_utilisateur:String,
     id_employe : String,
     dateHeureRDV: Date,
--- routes/employeRoute.js
+++ routes/employeRoute.js
@@ -1,6 +1,8 @@
 const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId
 
 const Employe = require('../models/backOffice/employeModel');
 const RendezVous = require('../models/backOffice/rendezVousModel');
@@ -37,10 +39,10 @@ router.get('/listeemploye', async (request, response) => {
 
 // route pour recuperer les rendez-vous par employe 
 router.get('/rendezVousEmploye/:idEmploye',async(request,response)=>{
-  const idEmploye = request.params.idEmploye ;
+  let idEmploye = request.params.idEmploye ;
     try{
-      const rdvByEmploye = await RendezVous.find({"id_employe":idEmploye});
-      if (rdvByEmploye.length > 0) {
+      const rdvByEmploye = await RendezVous.find({"id_employe":new ObjectId(idEmploye)});
+      if (rdvByEmploye) {
         const reponse = {
           message: 'OK',
           value: rdvByEmploye,
@@ -64,5 +66,38 @@ router.get('/rendezVousEmploye/:idEmploye',async(request,response)=>{
       response.status(500).json(rep);
     }
 });
+
+//route pour rechercher les rendez-vous par date et idEmploye(session)
+router.get('rendezVousParDate/:idEmploye/:dateRecherche',async(request,response)=>{
+    const idEmploye = request.params.idEmploye;
+    const dateRecherche = request.params.dateRecherche;
+    try {
+       const rechercheParDate = await RendezVous.find({"idEmploye": new ObjectId(idEmploye),"dateHeureRDV": dateRecherche})
+       if (rechercheParDate){
+        const reponse = {
+          message: 'OK',
+          value: rdvByEmploye,
+          code: 200,
+        };
+        response.json(reponse);
+       } else {
+        const rep = {
+          message: 'Aucun rendez vous',
+          code: 404,
+          value: null
+        };
+        response.status(404).json(rep);
+      }
+    }catch(err){
+      const rep = {
+        message: 'Erreur serveur',
+        code: 500,
+        value: err.message
+      };
+      response.status(500).json(rep);
+    }
+});
+
+// route pour ajouter une description du profil employes
   
 module.exports = router;
