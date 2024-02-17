f0ddaa481adb418951e351a30aa3fe2755787ad5
RAHAJASON Aroniaina Saotra <89457263+AroniainaSaotra@users.noreply.github.com>
2024-02-17 11:23:45 +0300

    ajout description profil employe

--- routes/employeRoute.js
+++ routes/employeRoute.js
@@ -8,13 +8,14 @@ const Employe = require('../models/backOffice/employeModel');
 const RendezVous = require('../models/backOffice/rendezVousModel');
 
 // Route pour récupérer la liste des employes
+//test
 router.get('/listeemploye', async (request, response) => {
     try {
       const employes = await Employe.find();
   
       if (employes.length > 0) {
         const reponse = {
-          message: 'OK',
+          message: 'liste des employes',
           value: employes,
           code: 200,
         };
@@ -39,19 +40,19 @@ router.get('/listeemploye', async (request, response) => {
 
 // route pour recuperer les rendez-vous par employe 
 router.get('/rendezVousEmploye/:idEmploye',async(request,response)=>{
-  let idEmploye = request.params.idEmploye ;
     try{
+      const idEmploye = request.params.idEmploye ;
       const rdvByEmploye = await RendezVous.find({"id_employe":new ObjectId(idEmploye)});
       if (rdvByEmploye) {
         const reponse = {
-          message: 'OK',
+          message: 'Liste rendez-vous des employes',
           value: rdvByEmploye,
           code: 200,
         };
         response.json(reponse);
       } else {
         const rep = {
-          message: 'Aucun rendez vous',
+          message: 'Aucun rendez vous pour cette employe',
           code: 404,
           value: null
         };
@@ -69,20 +70,20 @@ router.get('/rendezVousEmploye/:idEmploye',async(request,response)=>{
 
 //route pour rechercher les rendez-vous par date et idEmploye(session)
 router.get('rendezVousParDate/:idEmploye/:dateRecherche',async(request,response)=>{
-    const idEmploye = request.params.idEmploye;
-    const dateRecherche = request.params.dateRecherche;
     try {
+      const idEmploye = request.params.idEmploye;
+      const dateRecherche = request.params.dateRecherche;
        const rechercheParDate = await RendezVous.find({"idEmploye": new ObjectId(idEmploye),"dateHeureRDV": dateRecherche})
        if (rechercheParDate){
         const reponse = {
-          message: 'OK',
+          message: 'Rendez-vous par date OK',
           value: rdvByEmploye,
           code: 200,
         };
         response.json(reponse);
        } else {
         const rep = {
-          message: 'Aucun rendez vous',
+          message: 'Aucun rendez vous par date',
           code: 404,
           value: null
         };
@@ -98,6 +99,20 @@ router.get('rendezVousParDate/:idEmploye/:dateRecherche',async(request,response)
     }
 });
 
-// route pour ajouter une description du profil employes
-  
+// route pour ajouter une description du profil employe
+router.post('/employe/:id', async (request, response) => {
+  try {
+      const id = request.params.id;
+      const { description } = request.body;
+      let employe = await Employe.findById(id);
+      Employe.description = description;
+      await Employe.save();
+      return response.status(200).json({ message: "Description de l'employé mise à jour avec succès.", employe });
+  } catch (error) {
+      return response.status(500).json({ message: "Erreur serveur.", error: error.message });
+  }
+});
+
+
+
 module.exports = router;
