213fb5cd37bbec27e7b2aa4d39b2983cffbef5d4
RAHAJASON Aroniaina Saotra <89457263+AroniainaSaotra@users.noreply.github.com>
2024-02-18 17:11:34 +0300

    Chiffre d'affaire par jour

--- routes/managerRoute.js
+++ routes/managerRoute.js
@@ -8,7 +8,7 @@ const Employe = require('../models/backOffice/employeModel');
 const RendezVous = require('../models/backOffice/rendezVousModel');
 
 //nb de reservation par jour / mois
-router.get('/reservationParJour/count', async (request, response) => {
+router.get('/nbDeReservation', async (request, response) => {
     try {
         const { type } = request.query;
 
@@ -52,5 +52,68 @@ router.get('/reservationParJour/count', async (request, response) => {
         return response.status(500).json({ message: "Erreur serveur.", error: error.message });
     }
 });
+
+//Chiffre d'affaire par jour / mois
+router.get('/chiffre-affaires', async (request, response) => {
+    try {
+        const { type } = request.query;
+
+        let aggregateOptions;
+
+        if (type === 'day') {
+            // Agrégation par jour
+            aggregateOptions = [
+                {
+                    $lookup: {
+                        from: "Sous-Services",
+                        localField: "id_detail",
+                        foreignField: "_id",
+                        as: "details"
+                    }
+                },
+                {
+                    $group: {
+                        _id: {
+                            day: { $dayOfMonth: "$dateHeureRDV" },
+                            month: { $month: "$dateHeureRDV" },
+                            year: { $year: "$dateHeureRDV" }
+                        },
+                        chiffreAffaires: { $sum: { $sum: "$details.prix_detail" } }
+                    }
+                }
+            ];
+        } else if (type === 'month') {
+            // Agrégation par mois
+            aggregateOptions = [
+                {
+                    $lookup: {
+                        from: "Sous-Services",
+                        localField: "id_detail",
+                        foreignField: "_id",
+                        as: "details"
+                    }
+                },
+                {
+                    $group: {
+                        _id: {
+                            month: { $month: "$date_heure_rdv" },
+                            year: { $year: "$date_heure_rdv" }
+                        },
+                        chiffreAffaires: { $sum: { $sum: "$details.prix_detail" } }
+                    }
+                }
+            ];
+        } else {
+            return response.status(400).json({ message: "Type de période invalide. Veuillez spécifier 'day' ou 'month'." });
+        }
+
+        const results = await RendezVous.aggregate(aggregateOptions);
+
+        return response.status(200).json({ results });
+    } catch (error) {
+        return response.status(500).json({ message: "Erreur serveur.", error: error.message });
+    }
+});
+
 
 module.exports = router;
