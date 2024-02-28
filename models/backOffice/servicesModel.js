9b60240d5b84ba7890ee06970fe655f30c957c9e
rufimirana <razakambololonarufinah@gmail.com>
2024-02-28 14:45:12 +0300

    Update servicesModel.js

--- models/backOffice/servicesModel.js
+++ models/backOffice/servicesModel.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const servicesSchema = mongoose.Schema({
+<<<<<<< Updated upstream
     libelle_service:String,
     description_service:String
 })
 module.exports = mongoose.model('services',servicesSchema,'Services');
+=======
+  _id: String,
+  libelle_services: String,
+  description_services: String,
+});
+module.exports = mongoose.model("services", servicesSchema, "Services");
+>>>>>>> Stashed changes
